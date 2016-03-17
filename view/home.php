90151237480f7361450be14f48843f8ddde84941
Boyd220 <boydfranken@msn.com>
2016-03-17 09:31:02 +0100

    Skelet

--- view/home.php
+++ view/home.php
@@ -1,7 +1,7 @@
-<h2>Welkom op de feest app</h2>
+<h2>Welkom op de Hackathon app</h2>
 <div class="jumbotron">
 <div class="col-md-4">
 
-        <p> Schrijf u hieronder in om deel uit te kunnen maken van deze app </p>
+        <p> Hackathon app </p>
 </div>
 </div>
