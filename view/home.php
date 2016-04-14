7c948b6eeef3a4635126cf5fdc7a8ca40a64c3a6
Boyd220 <boydfranken@msn.com>
2016-04-14 16:09:13 +0200

    Stable

--- view/home.php
+++ view/home.php
@@ -1,7 +1,7 @@
-<h2>Welkom op de Hackathon app</h2>
+<h2>Welkom op de feest app</h2>
 <div class="jumbotron">
 <div class="col-md-4">
 
-        <p> Hackathon app </p>
+        <p> Schrijf u hieronder in om deel uit te kunnen maken van deze app </p>
 </div>
 </div>
---
+++ view/map.php
@@ -0,0 +1,15 @@
+<div class="col-md-3">
+ 	<button type="button" style="margin-top: 2em;" id="btnToggles" class="btn btn-danger fa">Toggle map</button>
+ 	<br>
+ 	<button type="button" style="margin-top: 2em;" id="btnToggles" class="btn btn-danger fa">Login user</button>
+ 	<br>
+ 	<button type="button" style="margin-top: 2em;" id="btnToggles" class="btn btn-danger fa">Login party organizer</button>
+ 	<br>
+ 	<button type="button" style="margin-top: 2em;" id="btnToggles" class="btn btn-danger fa">Buy premium</button>
+ 	<br>
+ </div>
+<div class="col-md-9 vertical-center">
+      <input id="pac-input" class="controls" type="text" placeholder="Search Box">
+      <div style="margin-top:5em;" id="map"></div>
+</div>
+
