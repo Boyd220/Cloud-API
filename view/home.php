2cca9e800e9461c51b70b0414b4fd054420d0d63
Boyd220 <boydfranken@msn.com>
2016-04-18 16:03:10 +0200

    Stable

--- view/home.php
+++ view/home.php
@@ -1,19 +1,6 @@
-<script>
-var ref = new Firebase("https://crackling-torch-6492.firebaseio.com/");
 
-$('#id').on('click', function(){
-ref.authWithOAuthPopup("facebook", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-    alert("Aangemeld met " authData.provider, authData.uid);	
-  }
-});
-});
-</script>
 <h2>Welkom op de feest app</h2>
-<button id="id">Login fb</button>
+
 <div class="jumbotron">
 <div class="col-md-4">
 
