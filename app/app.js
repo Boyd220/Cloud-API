65c8c90535aec929152b6e0574e528426b5f5eaa
Boyd220 <boydfranken@msn.com>
2016-05-02 10:55:51 +0200

    Set up server and can add party

--- app/app.js
+++ app/app.js
@@ -20,7 +20,12 @@ sampleApp.config(['$routeProvider',
         templateUrl: 'view/register.html'
       }).
         when('/Premium', {
-        templateUrl: 'premium.php'}).
+        templateUrl: 'premium.php'
+      }).
+        when('/AddParty',{
+          templateUrl: 'view/AddParty.html',
+          controller: 'partyController'
+        }).
       otherwise({
         redirectTo: '/Home'
       });
@@ -28,12 +33,7 @@ sampleApp.config(['$routeProvider',
  
  
 sampleApp.controller('mainController', function($scope) {
-     
-     $scope.showDiv = '1';
 
-    $scope.toggleShow = function(){
-       $scope.showDiv = !$scope.showDiv;
-    } 
 });
 
 
@@ -174,6 +174,15 @@ loadMarkers();
       }
 
       function loadMarkers(){
+
+        var ref = new Firebase("https://crackling-torch-6492.firebaseio.com/feestjes");
+// Retrieve new posts as they are added to our database
+ref.on("child_added", function(snapshot, prevChildKey) {
+  var newPost = snapshot.val();
+  console.log("Lat: " + newPost.lat);
+  
+
+
         var markerPositionDour = new google.maps.LatLng(50.39583, 3.77792);
 
 
@@ -343,10 +352,142 @@ markerRW = new google.maps.Marker({
  //sets the z-index of the map marker.
  zIndex:102
 });
+});
       };
 
 initAutocomplete();
 
   });
+sampleApp.controller('partyController', function($scope) {
+
+ var myDataRef = new Firebase('https://crackling-torch-6492.firebaseio.com/');
+$("#partyAdd").on("click", function(){
+          var uid = Math.floor((Math.random() * 1000000000000) + 1);
+          var feestNaam = $('#feestNaam').val();
+          var organisator= $('#organisator').val(); 
+          var Latitude = $('#longval').val();
+          var Longitude = $('#latval').val();
+          var Datum =$('#datum').val();
+
+    myDataRef.child("feestjes").child(uid).set({
+      name: feestNaam,
+      organisor: organisator,
+      lat: Latitude,
+      lng: Longitude,
+      date:Datum
+    }, function(error, partyData){;
+          if (error) 
+      {
+        console.log("Error creating party:", error);
+      } 
+      else 
+      {
+      alert("Party created", partyData)
+      }
+    });
+});
+
+    var json = (function () { 
+            var json = null; 
+                $.ajax({ 
+                    'async': false, 
+                    'global': true, 
+                    'url': "http://localhost:3000/", 
+                    'dataType': "json", 
+                    'success': function (data) {
+                     json = data; } }); 
+                return json;
+         })();
+$scope.json = json;
+
+console.log($scope.json);
+
+
+
+var gmapdata;
+var gmapmarker;
+var infoWindow;
+
+var def_zoomval = 10;
+var def_longval = 4.4029;
+var def_latval = 51.2192;
+
+function if_gmap_init()
+{
+  var curpoint = new google.maps.LatLng(def_latval,def_longval);
+
+  gmapdata = new google.maps.Map(document.getElementById("mapitems"), {
+    center: curpoint,
+    zoom: def_zoomval,
+    mapTypeId: 'roadmap'
+    });
+
+  gmapmarker = new google.maps.Marker({
+          map: gmapdata,
+          position: curpoint
+        });
+
+  infoWindow = new google.maps.InfoWindow;
+  google.maps.event.addListener(gmapdata, 'click', function(event) {
+    document.getElementById("longval").value = event.latLng.lng().toFixed(6);
+    document.getElementById("latval").value = event.latLng.lat().toFixed(6);
+    gmapmarker.setPosition(event.latLng);
+    if_gmap_updateInfoWindow();
+  });
+
+  google.maps.event.addListener(gmapmarker, 'click', function() {
+    if_gmap_updateInfoWindow();
+    infoWindow.open(gmapdata, gmapmarker);
+  });
+
+  document.getElementById("longval").value = def_longval;
+  document.getElementById("latval").value = def_latval;
+
+  return false;
+} // end of if_gmap_init
+
+
+function if_gmap_loadpicker()
+{
+  var longval = document.getElementById("longval").value;
+  var latval = document.getElementById("latval").value;
+
+  if (longval.length > 0) {
+    if (isNaN(parseFloat(longval)) == true) {
+      longval = def_longval;
+    } // end of if
+  } else {
+    longval = def_longval;
+  } // end of if
+
+  if (latval.length > 0) {
+    if (isNaN(parseFloat(latval)) == true) {
+      latval = def_latval;
+    } // end of if
+  } else {
+    latval = def_latval;
+  } // end of if
+
+  var curpoint = new google.maps.LatLng(latval,longval);
+
+  gmapmarker.setPosition(curpoint);
+  gmapdata.setCenter(curpoint);
+  //gmapdata.setZoom(zoomval);
+
+  if_gmap_updateInfoWindow();
+  return false;
+} // end of if_gmap_loadpicker
+
+
+
+function if_gmap_updateInfoWindow()
+{
+  infoWindow.setContent("Longitude: "+ gmapmarker.getPosition().lng().toFixed(6)+"<br>"+"Latitude: "+ gmapmarker.getPosition().lat().toFixed(6));
+} // end of if_gmap_bindInfoWindow
+    
+if_gmap_init();
+  
+});
+
 
 
